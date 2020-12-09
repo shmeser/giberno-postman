79d8003ea10d67248e3e66aecfdb7a98bd952370
ShmelevS <sergey.shmelev@appcraft.team>
2020-12-09 12:52:56 +0300

    update backup.sh

--- devops/develop/backup.sh
+++ devops/develop/backup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-dir_id=1TioiW5YollsYnTLKiXSB9YuUS_-riVVB # ID директории на google-диске
+dir_id=1r1sFc6MSPs-BvkcG9-Pap1XBeUkYuL9R # ID директории на google-диске
 
-cd /srv/giberno-server_develop/ # переходим в папку с проектом
+PROJ_DIR=/srv/giberno-server_develop
+GDRIVE_DIR=$PROJ_DIR/devops
 
+cd $PROJ_DIR # переходим в папку с проектом
 
 postgres_db_archive_name=postgres-data-$(date +%Y-%m-%d.%H%M%S)
 files_archive_name=files-$(date +%Y-%m-%d.%H%M%S)
@@ -15,16 +17,16 @@ tar cfvj /tmp/${postgres_db_archive_name}.tar.bz2 ${postgres_db_path} # созд
 tar cfvj /tmp/${files_archive_name}.tar.bz2 ${files_path} # создаем архив с файлами
 
 # отпраляем архивы на google-диск
-gdrive upload  --service-account accounts.json  --delete --parent ${dir_id} /tmp/${postgres_db_archive_name}.tar.bz2
-gdrive upload  --service-account accounts.json  --delete --parent ${dir_id} /tmp/${files_archive_name}.tar.bz2
+$GDRIVE_DIR/gdrive upload  --config $GDRIVE_DIR --service-account /develop/gdrive-account.json  --delete --parent ${dir_id} /tmp/${postgres_db_archive_name}.tar.bz2
+$GDRIVE_DIR/gdrive upload  --config $GDRIVE_DIR --service-account /develop/gdrive-account.json  --delete --parent ${dir_id} /tmp/${files_archive_name}.tar.bz2
 
 # необходимо удалить с google-диска старые бекапы, чтобы не забивать место
 
 rotationDate=$(date -d "-3 day" +"%Y-%m-%d")
 echo $rotationDate
-files=$(gdrive list --no-header --service-account accounts.json --query "'$dir_id' in parents and modifiedTime < '$rotationDate'" | awk '{ print $1 }')
+files=$($GDRIVE_DIR/gdrive list --no-header --config $GDRIVE_DIR --service-account /develop/gdrive-account.json --query "'$dir_id' in parents and modifiedTime < '$rotationDate'" | awk '{ print $1 }')
 
 for file in $files
 do
-gdrive --service-account accounts.json delete $file
+$GDRIVE_DIR/gdrive --config $GDRIVE_DIR --service-account /develop/gdrive-account.json delete $file
 done
