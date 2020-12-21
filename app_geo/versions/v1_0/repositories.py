a1dd186e1475fee9e8a782e1984973a3deaaedbd
ShmelevS <sergey.shmelev@appcraft.team>
2020-12-21 15:18:59 +0300

    get nationalities

--- app_geo/versions/v1_0/repositories.py
+++ app_geo/versions/v1_0/repositories.py
@@ -1,4 +1,4 @@
-from app_geo.models import Language
+from app_geo.models import Language, Country
 from backend.errors.enums import RESTErrors
 from backend.errors.http_exception import HttpException
 from backend.mixins import MasterRepository
@@ -6,6 +6,19 @@ from backend.mixins import MasterRepository
 
 class LanguagesRepository(MasterRepository):
     model = Language
+
+    def get_by_id(self, record_id):
+        try:
+            return self.model.objects.get(id=record_id, is_staff=False)
+        except self.model.DoesNotExist:
+            raise HttpException(
+                status_code=RESTErrors.NOT_FOUND.value,
+                detail='Объект %s с ID=%d не найден' % (self.model._meta.verbose_name, record_id)
+            )
+
+
+class CountriesRepository(MasterRepository):
+    model = Country
 
     def get_by_id(self, record_id):
         try:
