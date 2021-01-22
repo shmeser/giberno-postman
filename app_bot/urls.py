3678bd64c90208a219196431e4bbf6305e760ffa
ShmelevS <sergey.shmelev@appcraft.team>
2021-01-22 12:07:52 +0300

    fx

--- app_bot/urls.py
+++ app_bot/urls.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from app_bot.views import TelegramBotView, TestView
 
 urlpatterns = [
-    # Чтобы получать запросы от телеграма, в которые нельзя передать CSRF токер, необходимо использовать csrf_exempt
+    # Чтобы получать запросы от телеграма, в которые нельзя передать CSRF токен, необходимо использовать csrf_exempt
     path('telegram/webhooks', csrf_exempt(TelegramBotView.as_view())),
     path('telegram/webhooks/test', TestView.as_view()),
 ]
