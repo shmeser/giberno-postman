0bcc2f195f35ec0760b316614e4c50958fb47f08
ShmelevS <sergey.shmelev@appcraft.team>
2021-01-22 15:00:22 +0300

    rollback

--- app_users/versions/v1_0/repositories.py
+++ app_users/versions/v1_0/repositories.py
@@ -110,7 +110,6 @@ class AuthRepository:
 
         # Получаем или создаем пользователя
         defaults = {
-            'username': social_data.username,
             'phone': social_data.phone,
             'first_name': social_data.first_name,
             'last_name': social_data.last_name,
