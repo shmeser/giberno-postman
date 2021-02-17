ca215d02c4d60ce59f6b34af11bcdcde9b8ffb22
beng <benghayrapetyan@gmail.com>
2021-02-17 16:51:15 +0400

    signals

--- app_market/signals.py
+++ app_market/signals.py
@@ -7,6 +7,4 @@ from app_market.models import Vacancy
 @receiver(pre_save, sender=Vacancy)
 def set_timezone_to_vacancy(sender, instance, **kwargs):
     # Устанавливаем для вакансии часовой пояс из города, в котором она находится
-    # instance.timezone = instance.shop.city.timezone or 'Europe/Moscow'
-    print(instance)
-    print(type(instance))
+    instance.timezone = instance.shop.city.timezone or 'Europe/Moscow'
