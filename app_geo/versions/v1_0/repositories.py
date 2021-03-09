1032642fafa47e8067846e4a47200b528c954d94
ShmelevS <sergey.shmelev@appcraft.team>
2021-03-09 07:43:42 +0300

    fx

--- app_geo/versions/v1_0/repositories.py
+++ app_geo/versions/v1_0/repositories.py
@@ -1,5 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.gis.db.models import GeometryField, FilteredRelation, Q, Exists, OuterRef
+from django.contrib.gis.db.models import GeometryField
 from django.contrib.gis.db.models.functions import Distance, BoundingCircle
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models import Prefetch, F, ExpressionWrapper
@@ -123,7 +123,7 @@ class CitiesRepository(MasterRepository):
             F('population').desc(nulls_last=True)
         )
         if not within_boundary:
-            # Если не входит в зоны, то ищем ближайшие точки до которых менее NEAREST_POINT_DISTANCE_MAX метров
+            # Если не входит в зоны, то ищем ближайшие точки, до которых менее NEAREST_POINT_DISTANCE_MAX метров
             # Сортировка по возрастанию удаленности
             near_point = self.model.objects.annotate(distance=Distance('position', point)).filter(
                 distance__lte=NEAREST_POINT_DISTANCE_MAX).order_by('distance')
