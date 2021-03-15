fa911cbb44473242ad313322b1201e9d222731e9
ShmelevS <sergey.shmelev@appcraft.team>
2021-03-15 15:49:47 +0300

    store socket id

--- app_sockets/consumers.py
+++ app_sockets/consumers.py
@@ -1,5 +1,3 @@
-import logging
-
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from loguru import logger
 
@@ -18,7 +16,7 @@ class GroupConsumer(AsyncJsonWebsocketConsumer):
         self.socket_controller = None
         self.room_name = None
         self.room_id = None
-        self.room_group_name = None
+        self.group_name = None
 
     async def connect(self):
         self.socket_controller = AsyncSocketController(self)
@@ -27,24 +25,28 @@ class GroupConsumer(AsyncJsonWebsocketConsumer):
         self.room_id = chained_get(self.scope, 'url_route', 'kwargs', 'id')
         self.room_name = chained_get(self.scope, 'url_route', 'kwargs', 'room_name')
 
-        self.room_group_name = f'{self.room_name}{self.room_id}' if self.room_id and self.room_name else None
+        self.group_name = f'{self.room_name}{self.room_id}' if self.room_id and self.room_name else None
+
+        await self.accept()  # Принимаем соединение
 
         if self.user.is_authenticated:  # Проверка авторизации подключаемого соединения TODO проверка других разрешений
             # Добавляем соединение в группу
-            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-            await self.accept()  # Принимаем соединение
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+            await self.socket_controller.store_group_connection()
         else:
             # Принимаем соединение и сразу закрываем, чтобы не было ERR_CONNECTION_REFUSED
-            await self.accept()
             await self.close(code=SocketErrors.NOT_AUTHORIZED.value)  # Закрываем соединение с кодом НЕАВТОРИЗОВАН
 
     async def receive_json(self, content, **kwargs):
 
         logger.debug(content)
 
-        await self.channel_layer.group_send(self.room_group_name, {
+        await self.channel_layer.group_send(self.group_name, {
             'type': 'system_message',
-            'text': 'text'
+            'attachmentType': content['attachmentType'],
+            'attachmentPreviewUrl': content['attachmentPreviewUrl'],
+            'attachmentUrl': content['attachmentUrl'],
+            'text': content['text'],
         })
         # socket_event = SocketEventRM(content)
 
@@ -55,23 +57,24 @@ class GroupConsumer(AsyncJsonWebsocketConsumer):
         # if socket_event.event_type == SocketEventType.REGISTER_PROFILE:
         #     await self.register_profiles(socket_event)
 
+    async def disconnect(self, code):
+        try:
+            # Удаляем из группы
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
+            await self.socket_controller.disconnect()
+        except Exception as e:
+            logger.error(e)
+
     # # Системное - предупреждение, информация
     async def system_message(self, data):
         await self.send_json(
             {
+                'attachmentType': data['attachmentType'],
+                'attachmentPreviewUrl': data['attachmentPreviewUrl'],
+                'attachmentUrl': data['attachmentUrl'],
                 'text': data['text'],
             },
         )
-
-    async def disconnect(self, code):
-        try:
-            # Удаляем из группы
-            await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-            # await self.socket_controller.disconnect()
-        except SocketError as error:
-            await self.socket_controller.send_system_message(error.code, error.message)
-        except Exception as e:
-            print("DISCONNECT_ERROR", e)
 
 
 class Consumer(AsyncJsonWebsocketConsumer):
@@ -79,7 +82,6 @@ class Consumer(AsyncJsonWebsocketConsumer):
         super().__init__(*args, **kwargs)
         self.user = None
         self.socket_controller = None
-        self.logger = logging.getLogger(__name__)
 
     async def connect(self):
         self.socket_controller = AsyncSocketController(self)
@@ -87,48 +89,54 @@ class Consumer(AsyncJsonWebsocketConsumer):
 
         # TODO запрещать создание дублирующего соединения
 
+        already_connected = await self.socket_controller.check_if_connected()
+
+        await self.accept()  # Принимаем соединение
+
         if self.user.is_authenticated:  # Проверка авторизации подключаемого соединения
-            await self.accept()  # Принимаем соединение
+            if already_connected:
+                # Если уже есть соединение к этому роуту, то отклоняем новое соединение с ошибкой
+                await self.close(code=SocketErrors.FORBIDDEN.value)  # Закрываем соединение с кодом ЗАПРЕЩЕНО
+            else:
+                await self.socket_controller.store_single_connection()
         else:
-            # Принимаем соединение и сразу закрываем, чтобы не было ERR_CONNECTION_REFUSED
-            await self.accept()
+            # После установления сразу закрываем содинение, чтобы не было ERR_CONNECTION_REFUSED
             await self.close(code=SocketErrors.NOT_AUTHORIZED.value)  # Закрываем соединение с кодом НЕАВТОРИЗОВАН
 
     async def receive_json(self, content, **kwargs):
         logger.debug(content)
+
         socket_event = SocketEventRM(content)
 
         if socket_event.event_type == SocketEventType.LEAVE_GROUP:
             await self.leave_group(socket_event)
-            return
 
         if socket_event.event_type == SocketEventType.LOCATION:
             await self.update_location(socket_event)
 
-    async def leave_group(self, event: SocketEventRM):
+    async def disconnect(self, code):
         try:
-            self.user = await self.socket_controller.leave_group(event.token, event.group_name)
-            # socket_print(self.user, event.event_type, event.group_name)
-        except SocketError as error:
-            await self.socket_controller.send_system_message(error.code, error.message)
+            await self.socket_controller.disconnect()
+        except Exception as e:
+            logger.error(e)
 
     async def update_location(self, event: SocketEventRM):
         try:
             await self.channel_layer.send(self.channel_name, {
                 'type': 'location_updated',
-                'lat': 54,
-                'lon': 34,
+                'lat': event.lat,
+                'lon': event.lon,
             })
             # self.user = await self.socket_controller.update_location(event)
             # socket_print(self.user, event.event_type, "%s, %s" % (event.lon, event.lat))
         except SocketError as error:
             await self.socket_controller.send_system_message(error.code, error.message)
 
-    async def location_updated(self, event):
+    async def location_updated(self, data):
         await self.send_json(
             {
-                'lat': 54,
-                'lon': 34,
+                'lat': data['lat'],
+                'lon': data['lon']
             },
         )
 
@@ -139,11 +147,3 @@ class Consumer(AsyncJsonWebsocketConsumer):
                 'message': system['message'],
             },
         )
-
-    async def disconnect(self, code):
-        try:
-            await self.socket_controller.disconnect()
-        except SocketError as error:
-            await self.socket_controller.send_system_message(error.code, error.message)
-        except Exception as e:
-            print("DISCONNECT_ERROR", e)
