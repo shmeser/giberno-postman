80ff66659b82468589c14caca150cb410b087df5
ShmelevS <sergey.shmelev@appcraft.team>
2021-04-01 12:36:14 +0300

    correct import sequence

--- giberno/asgi.py
+++ giberno/asgi.py
@@ -1,25 +1,25 @@
 import os
 
-import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-# from app_sockets import routing
-# from app_sockets.middlewares import token_auth_middleware_stack
-
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giberno.settings')
-django.setup()
 django_asgi_app = get_asgi_application()
-
+# Подгружаем роутинг и авторизацию для сокетов после инициализации asgi
+from app_sockets import routing
+from app_sockets.middlewares import token_auth_middleware_stack
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
     "http": django_asgi_app,
 
     # WebSocket chat handler
-    # 'websocket': token_auth_middleware_stack(
-    #     URLRouter(
-    #         routing.websocket_urlpatterns
-    #     )
-    # ),
+    'websocket': token_auth_middleware_stack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
 })
