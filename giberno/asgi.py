a4f57f414a8466f0aee01cad96fdb33609bb441a
ShmelevS <sergey.shmelev@appcraft.team>
2021-04-01 11:55:26 +0300

    fx

--- giberno/asgi.py
+++ giberno/asgi.py
@@ -1,14 +1,14 @@
 import os
 
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-# import django
 from django.core.asgi import get_asgi_application
 
 from app_sockets import routing
 from app_sockets.middlewares import token_auth_middleware_stack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giberno.settings')
-# django.setup()
+django.setup()
 django_asgi_app = get_asgi_application()
 
 
