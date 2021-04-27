c53e0c6e701ed40d37ecf7a7e9f5427c7a88c3bf
ShmelevS <sergey.shmelev@appcraft.team>
2021-04-27 15:17:43 +0300

    new migrations

--- app_chats/enums.py
+++ app_chats/enums.py
@@ -4,3 +4,9 @@ from backend.enums import IntEnumM
 class ChatMessageType(IntEnumM):
     SIMPLE = 1  # Обычное текстовое сообщение с возможностью прикрепления файлов
     INFO = 2  # Информационное сообщение с иконкой
+
+
+class ChatManagerState(IntEnumM):
+    BOT_IS_USED = 0  # Общение с ботом
+    NEED_MANAGER = 1  # Необходим менеджер для ответа
+    MANAGER_CONNECTED = 2  # Менеджер подключен
