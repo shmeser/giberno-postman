ef63555bd89b91cd78460b284f59aa5adbfce29d
ShmelevS <sergey.shmelev@appcraft.team>
2021-04-30 12:58:15 +0300

    fx

--- app_market/utils.py
+++ app_market/utils.py
@@ -18,5 +18,7 @@ def handle_date_for_appeals(shift_active_date, time_object):
     month = str(shift_active_date.month)
     if len(month) == 1:
         month = '0' + month
-    day = shift_active_date.day
+    day = str(shift_active_date.day)
+    if len(day) == 1:
+        day = '0' + day
     return datetime.fromisoformat(f'{year}-{month}-{day} {time_object}')
