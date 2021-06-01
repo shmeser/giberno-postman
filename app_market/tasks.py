f158deccfb25f6a2dff8a8235ee587295fd4c8f5
beng <bengur110392@mail.ru>
2021-06-01 15:45:36 +0300

    init commit

--- app_market/tasks.py
+++ app_market/tasks.py
@@ -4,22 +4,27 @@ from celery import shared_task
 from django.db import transaction
 from django.utils import timezone
 
-from app_market.enums import ShiftAppealStatus
-from app_market.models import UserShift, ShiftAppeal
-from app_market.utils import QRHandler
-
-
-@shared_task
-def set_qr_code_to_user_shifts():
-    real_time_start = timezone.now() + timedelta(minutes=30)
-    queryset = UserShift.objects.filter(real_time_start=real_time_start)
-    with transaction.atomic():
-        for item in queryset:
-            item.qr_code = QRHandler(item).create_qr_data()
-            item.save()
+from app_market.enums import ShiftAppealStatus, JobStatus
+from app_market.models import ShiftAppeal
 
 
 @shared_task
 def cancel_overdue_appeals():
     queryset = ShiftAppeal.objects.filter(status=ShiftAppealStatus.INITIAL.value, time_start__lte=timezone.now())
     queryset.update(status=ShiftAppealStatus.CANCELED.value)
+
+
+@shared_task
+def notify_user_about_job_status():
+    confirmed_appeals = ShiftAppeal.objects.filter(status=ShiftAppealStatus.CONFIRMED)
+
+    soon = timezone.now() + timedelta(minutes=30)
+
+    with transaction.atomic():
+        for appeal in confirmed_appeals.filter(job_status__isnull=True):
+            if timezone.now() < appeal.time_start < soon:
+                appeal.job_status = JobStatus.JOB_SOON.value
+                appeal.save()
+                # уведомление - работа скоро
+
+    # TODO прикрутить логику по остальным статусам
