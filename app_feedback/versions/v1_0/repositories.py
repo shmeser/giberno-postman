716457c1d7da95314885523bbb9af50fda111571
ShmelevS <sergey.shmelev@appcraft.team>
2021-06-24 16:29:31 +0300

    reviews and socket event for completed job

--- app_feedback/versions/v1_0/repositories.py
+++ app_feedback/versions/v1_0/repositories.py
@@ -2,7 +2,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models import prefetch_related_objects, Prefetch
 
 from app_feedback.models import Review
-from app_market.models import Shop
+from app_market.models import Shop, Distributor, Vacancy
 from app_media.enums import MediaType, MediaFormat
 from app_media.models import MediaModel
 from app_users.models import UserProfile
@@ -12,15 +12,11 @@ from backend.mixins import MasterRepository
 class ReviewsRepository(MasterRepository):
     model = Review
 
-    def get_shop_reviews(self, shop_id, pagination=None):
-        target_ct = ContentType.objects.get_for_model(Shop)
-
+    def get_reviews(self, target_ct, target_ids):
         queryset = self.model.objects.filter(
             target_ct=target_ct,
-            target_id=shop_id,
+            target_id__in=target_ids,
             deleted=False
-        ).select_related(
-            'shift__vacancy'
         ).order_by('-created_at')
 
         # Префетчим логотипы магазинов для owner, который Generic Relation
@@ -52,6 +48,45 @@ class ReviewsRepository(MasterRepository):
                 ).order_by('-created_at'),  # Сортировка по дате обязательно
                 to_attr='medias'
             ))
+
+        return queryset
+
+    def get_shop_vacancies_reviews(self, shop_id, pagination=None):
+        target_ct = ContentType.objects.get_for_model(Vacancy)
+
+        vacancies_ids = Vacancy.objects.filter(
+            deleted=False,
+            shop_id=shop_id
+        ).values_list('id', flat=True)
+
+        queryset = self.get_reviews(target_ct, vacancies_ids)
+
+        if pagination:
+            return queryset[pagination.offset:pagination.limit]
+
+        return queryset
+
+    def get_distributor_reviews(self, distributor_id, pagination=None):
+        target_ct = ContentType.objects.get_for_model(Distributor)
+        queryset = self.get_reviews(target_ct, [distributor_id])
+
+        if pagination:
+            return queryset[pagination.offset:pagination.limit]
+
+        return queryset
+
+    def get_vacancy_reviews(self, vacancy_id, pagination=None):
+        target_ct = ContentType.objects.get_for_model(Vacancy)
+        queryset = self.get_reviews(target_ct, [vacancy_id])
+
+        if pagination:
+            return queryset[pagination.offset:pagination.limit]
+
+        return queryset
+
+    def get_self_employed_reviews(self, user_id, pagination=None):
+        target_ct = ContentType.objects.get_for_model(UserProfile)
+        queryset = self.get_reviews(target_ct, [user_id])
 
         if pagination:
             return queryset[pagination.offset:pagination.limit]
